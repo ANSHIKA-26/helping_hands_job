38acfbc4f3952fa29bdab483f2ca3f916347af2c
ANSHIKA-26 <114569262+ANSHIKA-26@users.noreply.github.com>
2024-07-15 13:31:46 +0530

    fix: corrs error

--- app.js
+++ app.js
@@ -41,5 +41,12 @@ app.use("/api/v1/job", jobRouter);
 
 app.use(errorMiddleware);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 
 export default app;
